65cb458ecce0b01fa5f3e91a479e4e3a519fd9b3
loopier <rogerpibernat@gmail.com>
2024-03-03 17:22:11 +0100

    Add more key bindings

--- emacs/ziva.el
+++ emacs/ziva.el
@@ -1,4 +1,4 @@
-;;; ziva.el --- Minor mode to code Ziva in Emacs     -*- lexical-binding: t; -*-
+;;; ziva.el --- Minor mode to code  t; -*-
 
 ;; Copyright (C) 2024  Roger Pibernat
 
@@ -40,6 +40,8 @@
 (add-hook 'ziva-mode-on-hook (lambda () (message "Ziva turned on")))
 (add-hook 'ziva-mode-off-hook (lambda () (message "Ziva turned off")))
 
+(setq ziva-mixer-next-available-track 0)
+
 ;; (map! :leader :n "o s" 'sclang-start :desc "Open Supercollider") ;; global under 'SPC-o' menu
 ;; (map! (:map ziva-mode-map :localleader :n "m" 'sclang-start))
 ;; (map! :map sclang-mode-map :localleader :n "z" 'ziva-livecoder)
@@ -47,16 +49,31 @@
 (map! :map ziva-mode-map :localleader :n "i" 'ziva-insert)
 
 (map! (:map sclang-mode-map :localleader :n "z b" 'ziva-boot))
-(map! (:map sclang-mode-map :localleader :n "t i t" 'ziva-insert-track))
+;; track
+(map! (:map sclang-mode-map :localleader :n "t i" 'ziva-insert-track))
+(map! (:map sclang-mode-map :localleader :n "t 1" 'ziva-insert-track-1))
+(map! (:map sclang-mode-map :localleader :n "t 2" 'ziva-insert-track-2))
+(map! (:map sclang-mode-map :localleader :n "t 3" 'ziva-insert-track-3))
+(map! (:map sclang-mode-map :localleader :n "t 4" 'ziva-insert-track-4))
+(map! (:map sclang-mode-map :localleader :n "t 5" 'ziva-insert-track-5))
+(map! (:map sclang-mode-map :localleader :n "t 6" 'ziva-insert-track-6))
+(map! (:map sclang-mode-map :localleader :n "t 7" 'ziva-insert-track-7))
+(map! (:map sclang-mode-map :localleader :n "t 8" 'ziva-insert-track-8))
+(map! (:map sclang-mode-map :localleader :n "t 9" 'ziva-insert-track-9))
 (map! (:map sclang-mode-map :localleader :n "t t" 'ziva-goto-track))
-(map! (:map sclang-mode-map :localleader :n "t a" 'ziva-goto-amp))
-(map! (:map sclang-mode-map :localleader :n "t f" 'ziva-goto-freq))
-(map! (:map sclang-mode-map :localleader :n "t o" 'ziva-goto-octave))
-(map! (:map sclang-mode-map :localleader :n "t l" 'ziva-goto-legato))
-(map! (:map sclang-mode-map :localleader :n "t k" 'ziva-goto-atk))
-(map! (:map sclang-mode-map :localleader :n "t d" 'ziva-goto-dec))
-(map! (:map sclang-mode-map :localleader :n "t s" 'ziva-goto-sus))
-(map! (:map sclang-mode-map :localleader :n "t r" 'ziva-goto-rel))
+;; track parameters
+(map! (:map sclang-mode-map :localleader :n "t I" 'ziva-goto-track-instrument))
+(map! (:map sclang-mode-map :localleader :n "t u" 'ziva-goto-track-dur))
+(map! (:map sclang-mode-map :localleader :n "t d" 'ziva-goto-track-degree))
+(map! (:map sclang-mode-map :localleader :n "t o" 'ziva-goto-track-octave))
+(map! (:map sclang-mode-map :localleader :n "t a" 'ziva-goto-track-amp))
+(map! (:map sclang-mode-map :localleader :n "t f" 'ziva-goto-track-freq))
+(map! (:map sclang-mode-map :localleader :n "t l" 'ziva-goto-track-legato))
+(map! (:map sclang-mode-map :localleader :n "t k" 'ziva-goto-track-atk))
+(map! (:map sclang-mode-map :localleader :n "t D" 'ziva-goto-track-dec))
+(map! (:map sclang-mode-map :localleader :n "t s" 'ziva-goto-track-sus))
+(map! (:map sclang-mode-map :localleader :n "t r" 'ziva-goto-track-rel))
+;; lfo
 (map! (:map sclang-mode-map :localleader :n "l v" 'ziva-lfo-value))
 (map! (:map sclang-mode-map :localleader :n "l s" 'ziva-lfo-sine))
 (map! (:map sclang-mode-map :localleader :n "l t" 'ziva-lfo-tri))
@@ -66,22 +83,47 @@
 (map! (:map sclang-mode-map :localleader :n "l n l" 'ziva-lfo-noise1))
 (map! (:map sclang-mode-map :localleader :n "l n e" 'ziva-lfo-noise2))
 (map! (:map sclang-mode-map :localleader :n "l f" 'ziva-lfo-freq))
-(map! (:map sclang-mode-map :localleader :n "l q" 'ziva-lfo-resonance))
-(map! (:map sclang-mode-map :localleader :n "l l i" 'ziva-lfo-line-out))
-(map! (:map sclang-mode-map :localleader :n "l l o" 'ziva-lfo-line-in))
+(map! (:map sclang-mode-map :localleader :n "l l" 'ziva-lfo-line))
+(map! (:map sclang-mode-map :localleader :n "l i" 'ziva-lfo-line-out))
+(map! (:map sclang-mode-map :localleader :n "l o" 'ziva-lfo-line-in))
 
 (defun ziva-boot () (interactive) (insert "Ziva.boot;"))
 
-(defun ziva-insert-track (name instrument mixer-track)
-  "Insert a new track with a NAME and INSTRUMENT to the MIXER-TRACK number."
-  (interactive "sName: \nsInstrument: \nnMixer Track: ")
-  (insert (format "~%s s: \\%s >>>.%d 1;" name instrument mixer-track)))
-
 (defun ziva-goto-track (name)
   (interactive "sTrack: ")
-  (if (search-forward-regexp (format "^~%s" name) nil t)
+  (setq x (search-forward-regexp (format "^~%s" name) nil t))
+  (if x
+      x
+    (search-backward-regexp (format "^~%s" name) nil t)))
+
+(defun ziva-insert-track (track instrument)
+  "Insert a new track with a NAME and INSTRUMENT."
+  (interactive "sName: \nsInstrument: ")
+  (if (search-forward-regexp "^$" nil t)
       nil
-    (search-backward-regexp (format "^~%s" name))))
+    (search-forward-regexp"$")
+  (insert (format "~%s s: \\%s >>>.%d 1;" track instrument ziva-mixer-next-available-track)))
+
+(defun ziva-insert-track-num (num)
+  (setq track (format "tk%d" num))
+  (if (ziva-goto-track track)
+      nil
+    (ziva-insert-track track "")
+    (move-beginning-of-line nil)
+    (search-forward "s: \\")
+    (move-to-column (- (current-column) 1))
+    (evil-append 1)))
+
+
+(defun ziva-insert-track-1 () (interactive) (ziva-insert-track-num 1))
+(defun ziva-insert-track-2 () (interactive) (ziva-insert-track-num 2))
+(defun ziva-insert-track-3 () (interactive) (ziva-insert-track-num 3))
+(defun ziva-insert-track-4 () (interactive) (ziva-insert-track-num 4))
+(defun ziva-insert-track-5 () (interactive) (ziva-insert-track-num 5))
+(defun ziva-insert-track-6 () (interactive) (ziva-insert-track-num 6))
+(defun ziva-insert-track-7 () (interactive) (ziva-insert-track-num 7))
+(defun ziva-insert-track-8 () (interactive) (ziva-insert-track-num 8))
+(defun ziva-insert-track-9 () (interactive) (ziva-insert-track-num 9))
 
 (defun ziva-goto-track-param (track param)
   "Go to tha TRACK's PARAMeter value, or create it if is doesn't exist."
@@ -102,92 +144,45 @@
     (search-forward ";")
     (goto-char (- (point 1)))))
 
-(defun ziva-goto-amp (track) (interactive "sTrack: \n") (ziva-goto-track-param track "amp"))
-(defun ziva-goto-freq (track) (interactive "sTrack: \n") (ziva-goto-track-param track "freq"))
-(defun ziva-goto-octave (track) (interactive "sTrack: \n") (ziva-goto-track-param track "octave"))
-(defun ziva-goto-legato (track) (interactive "sTrack: \n") (ziva-goto-track-param track "legato"))
-(defun ziva-goto-atk (track) (interactive "sTrack: \n") (ziva-goto-track-param track "atk"))
-(defun ziva-goto-dec (track) (interactive "sTrack: \n") (ziva-goto-track-param track "dec"))
-(defun ziva-goto-sus (track) (interactive "sTrack: \n") (ziva-goto-track-param track "sus"))
-(defun ziva-goto-rel (track) (interactive "sTrack: \n") (ziva-goto-track-param track "rel"))
+(defun ziva-goto-track-instrument (track) (interactive "sTrack: \n") (ziva-goto-track-param track "s"))
+(defun ziva-goto-track-dur (track) (interactive "sTrack: \n") (ziva-goto-track-param track "dur"))
+(defun ziva-goto-track-degree (track) (interactive "sTrack: \n") (ziva-goto-track-param track "degree"))
+(defun ziva-goto-track-octave (track) (interactive "sTrack: \n") (ziva-goto-track-param track "octave"))
+(defun ziva-goto-track-amp (track) (interactive "sTrack: \n") (ziva-goto-track-param track "amp"))
+(defun ziva-goto-track-freq (track) (interactive "sTrack: \n") (ziva-goto-track-param track "freq"))
+(defun ziva-goto-track-legato (track) (interactive "sTrack: \n") (ziva-goto-track-param track "legato"))
+(defun ziva-goto-track-atk (track) (interactive "sTrack: \n") (ziva-goto-track-param track "atk"))
+(defun ziva-goto-track-dec (track) (interactive "sTrack: \n") (ziva-goto-track-param track "dec"))
+(defun ziva-goto-track-sus (track) (interactive "sTrack: \n") (ziva-goto-track-param track "sus"))
+(defun ziva-goto-track-rel (track) (interactive "sTrack: \n") (ziva-goto-track-param track "rel"))
 
 
 (defun ziva-insert-lfo (name value)
-  (insert (format "~%s lfo: %s;" name value)))
+  (search-forward-regexp "$")
+  (insert (format "\n~%s lfo: %s;" name value))
+  (search-backward-regexp "(")
+  (evil-append 1))
 
-(defun ziva-lfo-func (name func freq)
-  (ziva-insert-lfo name (format "%s(%s, 0, 1)" func freq)))
+(defun ziva-lfo-func (name func)
+  (ziva-insert-lfo name (format "%s(, 0, 1)" func)))
 
-(defun ziva-lfo-sine (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "sine" freq))
-(defun ziva-lfo-tri (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "tri" freq))
-(defun ziva-lfo-saw (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "saw" freq))
-(defun ziva-lfo-pulse (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "pulse" freq))
-(defun ziva-lfo-noise0 (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "noise0" freq))
-(defun ziva-lfo-noise1 (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "noise1" freq))
-(defun ziva-lfo-noise2 (name freq) (interactive "sName: \nsFreq: \n") (ziva-lfo-func name "noise2" freq))
+(defun ziva-lfo-value (name value) (interactive "sName: \nsValue: ") (ziva-insert-lfo name value))
 
+(defun ziva-lfo-sine (name) (interactive "sName: ") (ziva-lfo-func name "sine"))
+(defun ziva-lfo-tri (name) (interactive "sName: ") (ziva-lfo-func name "tri"))
+(defun ziva-lfo-saw (name) (interactive "sName: ") (ziva-lfo-func name "saw"))
+(defun ziva-lfo-pulse (name) (interactive "sName: ") (ziva-lfo-func name "pulse"))
+(defun ziva-lfo-noise0 (name) (interactive "sName: ") (ziva-lfo-func name "noise0"))
+(defun ziva-lfo-noise1 (name) (interactive "sName: ") (ziva-lfo-func name "noise1"))
+(defun ziva-lfo-noise2 (name) (interactive "sName: ") (ziva-lfo-func name "noise2"))
 
-(defun ziva-agent (agent)
-  "Choose an agent.
-If an agent doesn't exist, it creates it.
-Otherwise it goes to it."
-  (interactive "sAgent: ")
-  (ziva-find-agent agent)
-  ;; (insert agent)
-  )
+(defun ziva-lfo-freq (name) (interactive "sName: ") (ziva-lfo-value name "sine(, 50, 15000)"))
+(defun ziva-lfo-line (name) (interactive "sName: ") (ziva-lfo-value name "line(, 0, 1)"))
+(defun ziva-lfo-line-in (name) (interactive "sName: ") (ziva-lfo-value name "line(, 0, 0.1)"))
+(defun ziva-lfo-line-out (name) (interactive "sName: ") (ziva-lfo-value name "line(, 0.1, 0)"))
 
-(defun ziva-find-agent (agent)
-  (copy-matching-lines agent))
-
-;; (setq ziva-tracks-map '((a "~t1 s: \\fmx >>>.1 1")
-;;                         (s "~t2 s: \\fmx >>>.2 1")
-;;                         (d "~t3 s: \\fmx >>>.3 1")
-;;                         (f "~t4 s: \\fmx >>>.4 1")
-;;                         (g "~t5 s: \\fmx >>>.5 1")
-;;                         (h "~t6 s: \\fmx >>>.6 1")
-;;                         (j "~t7 s: \\fmx >>>.7 1")
-;;                         (k "~t8 s: \\fmx >>>.8 1")
-;;                         ))
-
-;; (defun ziva-new-sound (snd)
-;;   "Create a new sound with a synth or a sample."
-;;   (interactive)))
-
-(defun ziva-get-command (cmd map)
-  "Get CMD from ziva-cmd-map."
-  (car (cdr (assoc (intern cmd) map))))
-
-(defun ziva-insert (cmd)
-  "Insert text CMD in buffer."
-  (interactive "sCommand: ")
-  (insert (ziva-get-command cmd ziva-tracks-map))
-  )
-
-(defun ziva-parse-cmds (cmds)
-  "Convert CMDS to code."
-  (let (chars result cmd-string)
-    (dolist (cmd cmds)
-      (setq cmd-string (ziva-get-command cmd))
-      (setq result (concat result cmd-string)))
-    result))
-
-(defun ziva-read-cmds ()
-  "Prompt for a command string."
-  (let (cmds cmd done)
-    (while (not done)
-      ;; (setq cmd (read-string (format "cmd %s: " cmds)))
-      (setq cmd (read-string (format "cmd %s: " (ziva-parse-cmds (reverse cmds)))))
-      (if (string-empty-p cmd)
-          (setq done t)
-        (push cmd cmds)))
-    (nreverse cmds)))
-
-(defun ziva-livecoder ()
-  "Livecoding assistant."
-  (interactive)
-  (insert (ziva-parse-cmds (ziva-read-cmds))))
-  ;; (ziva-run-cmd (ziva-read-cmds)))
-
+(defun ziva-increment
+    ())
 
 (provide 'ziva)
 ;;; ziva.el ends here
